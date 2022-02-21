9818779b1c1c156a258fdf3fd0bd51a64f8222ba
MickaelRigault <mick.rigault@gmail.com>
2022-02-21 09:40:38 +0100

    moving to version 2.0

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='typingapp',
     packages=['typingapp'],
+    version="0.2.0", # Version
     include_package_data=True,
     install_requires=[
         'flask',
