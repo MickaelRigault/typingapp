47df62519e6a15f3a74d3b6b944ae1966d9da5ad
Cloud User <rocky@ccwbvps19.in2p3.fr>
2022-03-10 13:06:58 +0100

    change app

--- typingapp/__init__.py
+++ typingapp/__init__.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "this_is_the_secretkey"
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-#app.run(debug=True, host='0.0.0.0')
 
 import typingapp.routes
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="8000")
