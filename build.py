94709e8628d941c97356c7de275b5769725bde41
MickaelRigault <mick.rigault@gmail.com>
2023-05-02 14:25:29 +0200

    update db

--- build.py
+++ build.py
@@ -2,9 +2,9 @@
 
 # Build the sql file if need and fills the target with the latests IDR
 
-from typingapp import app, io
+from typingapp import app, io, routes
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{io.DB_PATH}'
-_ = SQLAlchemy(app).create_all()
+_ = routes.db.create_all()
 _ = io.build_targets_db()
 
